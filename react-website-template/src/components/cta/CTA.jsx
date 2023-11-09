f6ae1332a338c6ffcec71f925dd1878fe382bb5e
marshal-brummel <mbrummel14@gmail.com>
2023-11-09 17:53:40 -0500

    Added Sample Call To Action Component

--- react-website-template/src/components/cta/CTA.jsx
+++ react-website-template/src/components/cta/CTA.jsx
@@ -3,7 +3,17 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='template__cta'>
+      <div className='template__cta-content'>
+        <p>Request material access</p>
+        <h3>
+          Register Today!
+        </h3>
+      </div>
+      <div className='template__cta-btn'>
+        <button type='button'>Register</button>
+      </div>
+    </div>
   )
 }
 
