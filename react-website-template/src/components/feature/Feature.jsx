cd18a1410df71a4ae90f94ae81ecd8075c18f7fe
marshal-brummel <mbrummel14@gmail.com>
2023-11-09 15:29:04 -0500

    Added Sample Feature Component

--- react-website-template/src/components/feature/Feature.jsx
+++ react-website-template/src/components/feature/Feature.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = () => {
+const Feature = ( {title, text} ) => {
   return (
-    <div>Feature</div>
+    <div className='template__features-container__feature'>
+      <div className='template__features-container__feature-title'>
+        <div/>
+        <h1>{title}</h1>
+      </div>
+      <div className='template__features-container__feature_text'>
+        <p>
+          {text}
+        </p>
+      </div>
+    </div>
   )
 }
 
--- react-website-template/src/container/whatTemplate/WhatTemplate.jsx
+++ react-website-template/src/container/whatTemplate/WhatTemplate.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatTemplate.css';
 
 const WhatTemplate = () => {
   return (
-    <div>WhatTemplate</div>
+    <div className='template__whatTemplate section__margin' id='whatTemplate'>
+      <div className='template__whatTemplate-feature'>
+        <Feature title="What is St. Patrick's?" text="Sample text for lots of content."/>
+      </div>
+      <div className='template__whatTemplate-heading'>
+        <h1 className='gradient__text'>
+          His mercy is endless.
+        </h1>
+        <p>
+          Sample text
+        </p>
+      </div>
+      <div className='template__whatTemplate-container'>
+        <Feature title="Cirriculum" text="Sample text for lots of content."/>
+        <Feature title="Pedagogy" text="Sample text for lots of content."/>
+        <Feature title="Support" text="Sample text for lots of content."/>
+      </div>
+    </div>
   )
 }
 
