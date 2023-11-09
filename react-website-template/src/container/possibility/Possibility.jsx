04305e45efbbc1720f5c7ad7a10c0ce83580718b
marshal-brummel <mbrummel14@gmail.com>
2023-11-09 17:40:24 -0500

    Added Sample Possibility Component

--- react-website-template/src/container/possibility/Possibility.jsx
+++ react-website-template/src/container/possibility/Possibility.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './possibility.css';
+import possibilityImage from '../../assets/possibility.png'
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='template__possibility section__padding' id='possibility'>
+      <div className='template__possibility-image'>
+        <img src={possibilityImage} alt='possibilityImage'/>
+      </div>
+      <div className='template__possibility-content'>
+        <h4>Request Access</h4>
+        <h1 className='gradient__text'>There are lots of cool things in this Cirriculum</h1>
+        <p>I swear its true!</p>
+      </div>
+    </div>
   )
 }
 
