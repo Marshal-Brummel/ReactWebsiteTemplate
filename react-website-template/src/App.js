1c91a73cec4cae5d8ef93d4d305e493fc902f464
marshal-brummel <mbrummel14@gmail.com>
2023-11-29 13:11:11 -0500

    Sticky Nav bar

--- react-website-template/src/App.js
+++ react-website-template/src/App.js
@@ -7,11 +7,9 @@ import './App.css';
 const App = () => {
   return (
     <div className='App'>
-      <div>
-        <Navbar />
-        <HeroBanner fullscreen={true} imgURL="https://assets-eu-01.kc-usercontent.com/aa24ba70-9a12-01ae-259b-7ef588a0b2ef/c550dd40-3a7c-4330-b4a0-6c3d70b7ceeb/header-st-patrick-s-cathedral-dublin-city.jpg?w=1280&h=480&fm=jpg&fit=crop" text="~ The Saint Patrick Curriculum* ~
+      <Navbar />
+      <HeroBanner fullscreen={true} imgURL="https://assets-eu-01.kc-usercontent.com/aa24ba70-9a12-01ae-259b-7ef588a0b2ef/c550dd40-3a7c-4330-b4a0-6c3d70b7ceeb/header-st-patrick-s-cathedral-dublin-city.jpg?w=1280&h=480&fm=jpg&fit=crop" text="~ The Saint Patrick Curriculum* ~
 Catholic, classical materials and training for PK - 8 teachers and schools "/>
-      </div>
       <Header />
       <Brand />
       <WhatTemplate />
